18f437b423bcb9d2e87f6d4809de84f14b2f8035
meldr <meldr@MSI.home>
2016-09-23 20:00:36 +0100

    New random method created

--- HomeworkPlus/HomeworkPlus/src/com/meldrum/utility/Random.java
+++ HomeworkPlus/HomeworkPlus/src/com/meldrum/utility/Random.java
@@ -10,4 +10,15 @@ public class Random {
 
     }
 
+    public static int randomNonZeroInt(int min, int max) {
+
+	int n = 0;
+
+	while (n == 0) {
+	    n = min + (int) (Math.random() * ((max - min) + 1));
+	}
+
+	return n;
+    }
+
 }
